d84bca9b4370a686476295721ba9a429b8ad354d
Rakesh Kumar <rakesh@techne-systems.com>
2024-12-26 18:29:14 -0600

    Unique

--- techne-extension Extension/Resources/background.js
+++ techne-extension Extension/Resources/background.js
@@ -1,5 +1,15 @@
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL('index.html')
-    });
+chrome.action.onClicked.addListener(async () => {
+    const url = chrome.runtime.getURL('index.html');
+    
+    // Find existing tab with our URL
+    const existingTabs = await chrome.tabs.query({ url });
+    
+    if (existingTabs.length > 0) {
+      // If tab exists, focus on it
+      await chrome.tabs.update(existingTabs[0].id, { active: true });
+      await chrome.windows.update(existingTabs[0].windowId, { focused: true });
+    } else {
+      // If no tab exists, create new one
+      await chrome.tabs.create({ url });
+    }
   });
