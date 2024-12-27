372a25eb1f902db456444e15f13d224ba0331ae0
Rakesh Kumar <rakesh@techne-systems.com>
2024-12-26 18:47:50 -0600

    opens in a tab

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require('path');
 
 module.exports = {
- entry: {
-   'main_page_content': './techne-extension Extension/Resources/src/main_page_content.ts',
-   'item_page_content': './techne-extension Extension/Resources/src/item_page_content.ts'
- },
- output: {
-   path: path.resolve(__dirname, 'techne-extension Extension/Resources'),
-   filename: '[name].js'
- },
- module: {
-   rules: [
-     {
-       test: /\.tsx?$/,
-       use: 'ts-loader',
-       exclude: /node_modules/
-     }
-   ]
- },
- resolve: {
-   extensions: ['.tsx', '.ts', '.js']
- },
- mode: 'production',
- devtool: 'cheap-module-source-map'
-
+  entry: {
+    'main_page_content': './techne-extension Extension/Resources/src/main_page_content.ts',
+    'item_page_content': './techne-extension Extension/Resources/src/item_page_content.ts',
+    'index': './techne-extension Extension/Resources/src/index.tsx' 
+  },
+  output: {
+    path: path.resolve(__dirname, 'techne-extension Extension/Resources'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom')
+    }
+  },
+  mode: 'production',
+  devtool: 'cheap-module-source-map'
 };
