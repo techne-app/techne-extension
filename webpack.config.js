2f11b948706ff364e018b70a72d63089f1465a65
Rakesh Kumar <rakesh@techne-systems.com>
2024-12-26 18:09:06 -0600

    index.html is a default_popup

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 module.exports = {
  entry: {
    'main_page_content': './techne-extension Extension/Resources/src/main_page_content.ts',
-   'item_page_content': './techne-extension Extension/Resources/src/item_page_content.ts',
-   'popup': './techne-extension Extension/Resources/src/index.tsx'
+   'item_page_content': './techne-extension Extension/Resources/src/item_page_content.ts'
  },
  output: {
    path: path.resolve(__dirname, 'techne-extension Extension/Resources'),
